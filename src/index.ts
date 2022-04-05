35326e86ee670a6b092f3545bc7f3010ac94e88b
Pierre-Luc Gagné <pierreluc212@gmail.com>
2022-04-06 03:32:46 +0900

    remove dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import http from "http";
-import "dotenv/config";
 import WordRouter from "./routes/WordRouter";
 import MemoryCacheServiceImpl from "./services/MemoryCacheServiceImpl";
 import HttpRequestServiceImpl from "./services/HttpRequestServiceImpl";
 import FastParseServiceImpl from "./services/FastParseServiceImpl";
 import WordService from "./services/WordServiceImpl";
+
+const PORT = process.env.PORT || 5000
 
 const memoryCacheService = new MemoryCacheServiceImpl();
 const HttpRequestService = new HttpRequestServiceImpl();
@@ -21,6 +22,6 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(process.env.PORT, () => {
-  console.log(`server started on port: ${process.env.PORT}`);
+server.listen(PORT, () => {
+  console.log(`server started on port: ${PORT}`);
 });
